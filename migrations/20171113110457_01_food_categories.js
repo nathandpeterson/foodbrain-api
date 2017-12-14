7b515592c29a20cf3b96cbadfc20a80424dae39f
Wes Reid <bwreid@gmail.com>
2017-12-14 10:46:34 -0800

    Update 20171113110457_01_food_categories.js

--- migrations/20171113110457_01_food_categories.js
+++ migrations/20171113110457_01_food_categories.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('food_categories', table => {
     table.increments()
     table.string('category').notNullable()
+    // What happened to timestamps?  :(
   })
 }
 
